cc0f9b638d289b8807e5cce61e6d34f85a7dd695
gwei3 <11015100@qq.com>
2021-06-30 23:11:57 +0800

    add initial grpc definitions for clientapi

--- attestation/ras/clientapi/clientapi.go
+++ attestation/ras/clientapi/clientapi.go
@@ -1,7 +1,108 @@
 package clientapi
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"time"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	port = "127.0.0.1:40001"
+)
+
+type server struct {
+	UnimplementedRasServer
+}
+
+func (s *server) CreateIKCert(ctx context.Context, in *CreateIKCertRequest) (*CreateIKCertReply, error) {
+	log.Printf("Received: %v", "CreateIKCert")
+	return &CreateIKCertReply{}, nil
+}
+
+func (s *server) RegisterClient(ctx context.Context, in *RegisterClientRequest) (*RegisterClientReply, error) {
+	log.Printf("Received: %v", "RegisterClient")
+	return &RegisterClientReply{}, nil
+}
+
+func (s *server) UnregisterClient(ctx context.Context, in *UnregisterClientRequest) (*UnregisterClientReply, error) {
+	log.Printf("Received: %v", "UnregisterClient")
+	return &UnregisterClientReply{}, nil
+}
+
+func (s *server) SendHeartbeat(ctx context.Context, in *SendHeartbeatRequest) (*SendHeartbeatReply, error) {
+	log.Printf("Received: %v", "SendHeartbeat")
+	return &SendHeartbeatReply{}, nil
+}
+
+func (s *server) SendReport(ctx context.Context, in *SendReportRequest) (*SendReportReply, error) {
+	log.Printf("Received: %v", "SendReport")
+	return &SendReportReply{}, nil
+}
+
+func startServer() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	RegisterRasServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+func startClient() {
+	conn, err := grpc.Dial(port, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := NewRasClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	_, err = c.CreateIKCert(ctx, &CreateIKCertRequest{})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("CreateIKCert ok")
+
+	_, err = c.RegisterClient(ctx, &RegisterClientRequest{})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("RegisterClient ok")
+
+	_, err = c.UnregisterClient(ctx, &UnregisterClientRequest{})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("UnregisterClient ok")
+
+	_, err = c.SendHeartbeat(ctx, &SendHeartbeatRequest{})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("SendHeartbeat ok")
+
+	_, err = c.SendReport(ctx, &SendReportRequest{})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("SendReport ok")
+}
 
 func Test() {
 	fmt.Println("hello, this is clientapi!")
+	fmt.Println("start ras server!")
+	go startServer()
+
+	fmt.Println("start ras client!")
+	startClient()
 }
--- attestation/ras/go.mod
+++ attestation/ras/go.mod
@@ -1,3 +1,8 @@
 module gitee.com/openeuler/kunpengsecl/attestation/ras
 
 go 1.15
+
+require (
+	google.golang.org/grpc v1.39.0
+	google.golang.org/protobuf v1.27.1
+)
