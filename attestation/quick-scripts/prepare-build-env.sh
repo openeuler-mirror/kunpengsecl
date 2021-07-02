09f42f4f23a2bfaf2ab7dd9bea3172539ac8305f
gwei3 <11015100@qq.com>
2021-07-02 17:30:11 +0800

    fix deps install

--- attestation/quick-scripts/prepare-build-env.sh
+++ attestation/quick-scripts/prepare-build-env.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 
-sudo dnf install golang
+sudo dnf install -y make golang
 go env -w GOPROXY="https://goproxy.io,direct"
 go env -w GO111MODULE="on"
 go get github.com/golangci/golangci-lint/cmd/golangci-lint@v1.41.1
