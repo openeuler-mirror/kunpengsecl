27ba441dbf8150befc8928b578e59bbdb691376c
aaron-liwang <3214053332@qq.com>
2021-10-30 14:20:54 +0800

    Do what ras tells to do by NextAction and complete the optimization of condition judgment statement of action instruction in heartbeat reply. Put action function in raagent package.

--- attestation/rac/cmd/raagent/main.go
+++ attestation/rac/cmd/raagent/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"time"
+
+	"gitee.com/openeuler/kunpengsecl/attestation/rac/ractools"
 
 	"gitee.com/openeuler/kunpengsecl/attestation/ras/clientapi"
 )
@@ -13,16 +16,16 @@ func main() {
 
 	//akname,_,akpub,err:=ractools.CreateAk(rw,parentHandle,"",ractools.MyPassword,ractools.PcrSelection1_17)
 	req := clientapi.CreateIKCertRequest{
-		EkPub: nil,
-		IkPub: nil,
+		EkPub:  nil,
+		IkPub:  nil,
 		IkName: nil,
 	}
-	bk,err := clientapi.DoCreateIKCert(addr,&req)
-	if err !=nil{
+	bk, err := clientapi.DoCreateIKCert(addr, &req)
+	if err != nil {
 		log.Fatal("Client:can't Create IkCert")
 	}
 	eic := bk.GetIcEncrypted()
-	log.Println("Client:get encryptedIC=",eic)
+	log.Println("Client:get encryptedIC=", eic)
 	// step 2. if rac doesn't have clientId, it uses Cert to do the register process.
 	cid := int64(-1)
 	if cid == -1 {
@@ -43,10 +46,62 @@ func main() {
 		log.Printf("Client: get heart beat back %v", bk.GetNextAction())
 
 		// step 4. do what ras tells to do by NextAction...
-
+		result := DoNextAction(bk, cid, addr)
+		if result {
+			log.Printf("do next action ok.")
+		} else {
+			log.Printf("do next action failed.")
+		}
 		// step 5. what else??
 
 		// step n. sleep and wait.
 		time.Sleep(time.Second * 3)
 	}
+}
+
+//具体的动作函数
+func ClearCommands(in1 interface{}, in2 interface{}, in3 interface{}) bool {
+	return true
+}
+
+func SendNewConf(in1 interface{}, in2 interface{}, in3 interface{}) bool {
+	fmt.Printf("send new configuration to RAC.")
+	return true
+}
+
+func GetNewTrurp(in1 interface{}, in2 interface{}, in3 interface{}) bool {
+	fmt.Printf("get a new trust report from RAC.")
+	rtr, err := ractools.GetTrustReport(nil, ractools.TrustReportIn{})
+	srr, _ := clientapi.DoSendReport(*in3.(*string), &clientapi.SendReportRequest{
+		ClientId:    *in2.(*int64),
+		TrustReport: nil,
+	})
+	if err != nil {
+		log.Fatalf("create a new trust report error!%v", err)
+	}
+	if srr.Result {
+		log.Printf("get a new trust report from RAC ok.%v", rtr)
+	} else {
+		log.Printf("get a new trust report from RAC failed.")
+	}
+	return true
+}
+
+func SendconfAndGetrep(in1 interface{}, in2 interface{}, in3 interface{}) bool {
+	_ = SendNewConf(in1, in2, in3)
+	_ = GetNewTrurp(in1, in2, in3)
+	return true
+}
+
+func DoNextAction(shbr *clientapi.SendHeartbeatReply, cid int64, addr string) bool {
+	var nCases = map[int]func(in1 interface{}, in2 interface{}, in3 interface{}) bool{
+		0: ClearCommands,
+		1: SendNewConf,
+		2: GetNewTrurp,
+		3: SendconfAndGetrep,
+	}
+	if nCases[int(shbr.GetNextAction())] == nil {
+		return false
+	}
+	return nCases[int(shbr.GetNextAction())](shbr, cid, addr)
 }
