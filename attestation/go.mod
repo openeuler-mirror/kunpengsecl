13de2d916b1ec93ed1a3c5c1d392950fc09950f4
wucaijun2001 <wucaijun2001@163.com>
2021-12-01 23:35:04 +0800

    implement the basic command line structure for tbprovisioner.

--- attestation/go.mod
+++ attestation/go.mod
@@ -5,7 +5,7 @@ go 1.15
 require (
 	github.com/deepmap/oapi-codegen v1.8.1
 	github.com/getkin/kin-openapi v0.61.0
-	github.com/golang/protobuf v1.5.0
+    github.com/golang/protobuf v1.5.0 // indirect
 	github.com/golangci/golangci-lint v1.41.1 // indirect
 	github.com/google/go-tpm v0.3.2
 	github.com/google/go-tpm-tools v0.2.1
@@ -13,10 +13,11 @@ require (
 	github.com/labstack/echo/v4 v4.3.0
 	github.com/lestrrat-go/jwx v1.2.9
 	github.com/pkg/errors v0.9.1
+	github.com/spf13/cobra v1.1.3
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.7.0
 	google.golang.org/grpc v1.40.0
 	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0 // indirect
-	google.golang.org/protobuf v1.26.0 // indirect
+	google.golang.org/protobuf v1.26.0
 )
---
+++ attestation/rac/cmd/tbprovisioner/cmd/root.go
@@ -0,0 +1,86 @@
+/*
+Copyright (c) Huawei Technologies Co., Ltd. 2021.
+kunpengsecl licensed under the Mulan PSL v2.
+You can use this software according to the terms and conditions of
+the Mulan PSL v2. You may obtain a copy of Mulan PSL v2 at:
+    http://license.coscl.org.cn/MulanPSL2
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+See the Mulan PSL v2 for more details.
+
+Author: wucaijun
+Create: 2021-12-01
+Description: Command line tool for tpm provision process.
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "tbprovisioner",
+	Short: "command line tool for tpm provision process.",
+	Long: `Command line tool for tpm provision process:
+1. read/write EK from/to TPM NVRAM,
+2. create EK and signed by remote attestation server,
+3. create IK and signed by remote attestation server,
+...
+`,
+
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Hi, I'm here! cmd=%v, args=%v", cmd, args)
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	cobra.CheckErr(rootCmd.Execute())
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.tbprovisioner.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".tbprovisioner" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".tbprovisioner")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
