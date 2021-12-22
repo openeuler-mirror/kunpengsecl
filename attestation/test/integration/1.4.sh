7f3d3cb2c197a67a98e16a324dd6d8645251387c
jiayunhao <963410578@qq.com>
2021-12-22 19:38:02 +0800

    add story1.4 test script

---
+++ attestation/test/integration/1.4.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# this scripts should be run under the root folder of kunpengsecl project
+#set -eux
+PROJROOT=.
+# run number of rac clients to test
+NUM=1
+# include common part
+. ${PROJROOT}/attestation/test/integration/common.sh
+
+# above are common preparation steps, below are specific preparation step, scope includs:
+# configure files, input files, environment variables, cmdline paramenters, flow control paramenters, etc.
+### Start Preparation
+echo "start test preparation..." | tee -a ${DST}/control.txt
+pushd $(pwd)
+cd ${PROJROOT}/attestation/quick-scripts
+echo "clean database" | tee -a ${DST}/control.txt
+sh clear-database.sh | tee -a ${DST}/control.txt
+popd
+### End Preparation
+
+### start launching binaries for testing
+echo "start ras..." | tee -a ${DST}/control.txt
+( cd ${DST}/ras ; ./ras -T &>${DST}/ras/echo.txt ; ./ras &>>${DST}/ras/echo.txt ;)&
+echo "wait for 3s"
+sleep 3
+
+# start number of rac clients
+echo "start ${NUM} rac clients..." | tee -a ${DST}/control.txt
+(( count=0 ))
+for (( i=1; i<=${NUM}; i++ ))
+do
+    ( cd ${DST}/rac-${i} ; ${DST}/rac/raagent -t &>${DST}/rac-${i}/echo.txt ; )&
+    (( count++ ))
+    if (( count >= 1 ))
+    then
+        (( count=0 ))
+        echo "start ${i} rac clients at $(date)..." | tee -a ${DST}/control.txt
+    fi
+done
+
+### start monitoring and control the testing
+echo "start to perform test ${TEST_ID}..." | tee -a ${DST}/control.txt
+echo "wait for 1min"
+sleep 60
+
+### stop testing
+echo "kill all test processes..." | tee -a ${DST}/control.txt
+pkill -u ${USER} ras
+pkill -u ${USER} raagent
+
+echo "test DONE!!!" | tee -a ${DST}/control.txt
+
+# Read the running log of rac
+ans=$(cat ${DST}/rac-$((i-1))/echo.txt | awk '/create a new trust report, succeeded/ {gsub("clientID=","",$7);print $1}')
+### analyse the testing data
+### generate the test report
+if [ "$ans" != "" ]
+then
+    echo "create trust report succeeded!" | tee -a ${DST}/control.txt
+    echo "test succeeded!" | tee -a ${DST}/control.txt
+    exit 0
+else
+    echo "create trust report failed!" | tee -a ${DST}/control.txt
+    echo "test failed!" | tee -a ${DST}/control.txt
+    exit 1
+fi
