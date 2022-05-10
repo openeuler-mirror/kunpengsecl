8f0cf6f7dbcecf65934e294f591d3c44a43acc44
aaron-liwang <3214053332@qq.com>
2022-05-10 11:15:18 +0000

    Add compilation of Verifier lib.

--- attestation/tee/demo/qca_demo/qcatools/qcatools.go
+++ attestation/tee/demo/qca_demo/qcatools/qcatools.go
@@ -75,14 +75,14 @@ func GetTAReport(ta_uuid int64, usr_data *Go_ra_buffer_data, report *Go_ra_buffe
 	c_usr_data.size = C.__uint32_t(usr_data.Size)
 	str_usrdata_buf = string(usr_data.Buf)
 	c_usrdata_buf = C.CString(str_usrdata_buf)
-	temp_buf0 := unsafe.Pointer(c_usrdata_buf)
-	c_usr_data.buf = (*C.uchar)(temp_buf0)
+	defer C.free(unsafe.Pointer(c_usrdata_buf))
+	c_usr_data.buf = (*C.uchar)(unsafe.Pointer(c_usrdata_buf))
 
 	c_report.size = C.__uint32_t(report.Size)
 	str_report_buf = string(report.Buf)
 	c_report_buf = C.CString(str_report_buf)
-	temp_buf1 := unsafe.Pointer(c_report_buf)
-	c_report.buf = (*C.uchar)(temp_buf1)
+	defer C.free(unsafe.Pointer(c_report_buf))
+	c_report.buf = (*C.uchar)(unsafe.Pointer(c_report_buf))
 
 	c_with_tcb = C.bool(with_tcb)
 
@@ -90,9 +90,8 @@ func GetTAReport(ta_uuid int64, usr_data *Go_ra_buffer_data, report *Go_ra_buffe
 
 	// format conversion: C -> Go
 	go_report.Size = uint32(c_ra_buffer_data.size)
-	temp_buf2 := unsafe.Pointer(c_ra_buffer_data.buf)
 
-	go_report.Buf = []byte(C.GoString((*C.char)(temp_buf2)))
+	go_report.Buf = []byte(C.GoString((*C.char)(unsafe.Pointer(c_ra_buffer_data.buf))))
 
 	return go_report
 }
