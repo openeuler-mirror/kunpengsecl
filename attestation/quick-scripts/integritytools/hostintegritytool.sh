0127de80d567d0290f7098807dbd50bee57657c0
gwei3 <11015100@qq.com>
2022-08-16 20:05:42 +0800

    update hostintegritytool.sh to fix grub regeneration

--- attestation/quick-scripts/integritytools/hostintegritytool.sh
+++ attestation/quick-scripts/integritytools/hostintegritytool.sh
@@ -15,12 +15,6 @@ else
 fi
 check_results=`uname -a | grep -i "ubuntu"`
 echo "command(uname) result is: $check_results"
-if [ -z "$check_results" ]
-then
-  path=/etc/selinux/config
-  selinux=`sed -rn "/^(SELINUX=).*\$/p" $path`
-  sed -ri "s@^(SELINUX=).*\$@\1enforcing@g" $path
-  touch /.autorelabel
   cat>/etc/ima/ima-policy<<EOF
 dont_measure fsmagic=0x9fa0
 dont_measure fsmagic=0x62656572
@@ -28,6 +22,19 @@ dont_measure fsmagic=0x64626720
 dont_measure fsmagic=0x01021994
 dont_measure fsmagic=0x858458f6
 dont_measure fsmagic=0x73636673
+measure func=FILE_MMAP mask=MAY_EXEC 
+measure func=BPRM_CHECK
+measure func=MODULE_CHECK uid=0
+measure func=PATH_CHECK mask=MAY_READ uid=0
+EOF
+sed -i '$a GRUB_CMDLINE_LINUX="$GRUB_CMDLINE_LINUX ima ima_template=ima-ng ima_hash=sha256"' /etc/default/grub
+if [ -z "$check_results" ]
+then
+  path=/etc/selinux/config
+  selinux=`sed -rn "/^(SELINUX=).*\$/p" $path`
+  sed -ri "s@^(SELINUX=).*\$@\1enforcing@g" $path
+  touch /.autorelabel
+  cat>/etc/ima/ima-policy<<EOF
 dont_measure obj_type=fsadm_log_t
 dont_measure obj_type=rsync_log_t
 dont_measure obj_type=wtmp_t
@@ -39,33 +46,10 @@ dont_measure obj_type=initrc_var_log_t
 dont_measure obj_type=lastlog_t
 dont_measure obj_type=nscd_log_t
 dont_measure obj_type=var_log_t
-measure func=FILE_MMAP mask=MAY_EXEC 
-measure func=BPRM_CHECK
-measure func=MODULE_CHECK uid=0
-measure func=PATH_CHECK mask=MAY_READ uid=0
 EOF
-sed -i '$a GRUB_CMDLINE_LINUX="ima"' /etc/default/grub
-sed -i '$a GRUB_CMDLINE_LINUX="ima_template=ima-ng"' /etc/default/grub
-sed -i '$a GRUB_CMDLINE_LINUX="ima_hash=sha256"' /etc/default/grub
-grub2-mkconfig -o /boot/grub2/grub.cfg
+grub2-mkconfig -o /boot/efi/EFI/openEuler/grub.cfg
 else
-  cat>/etc/ima/ima-policy<<EOF
-dont_measure fsmagic=0x9fa0
-dont_measure fsmagic=0x62656572
-dont_measure fsmagic=0x64626720
-dont_measure fsmagic=0x01021994
-dont_measure fsmagic=0x858458f6
-dont_measure fsmagic=0x73636673
-measure func=FILE_MMAP mask=MAY_EXEC
-measure func=BPRM_CHECK
-measure func=MODULE_CHECK uid=0
-measure func=PATH_CHECK mask=MAY_READ uid=0
-EOF
-  sed -i '$a GRUB_CMDLINE_LINUX="ima"' /etc/default/grub
-  sed -i '$a GRUB_CMDLINE_LINUX="ima_template=ima-ng"' /etc/default/grub
-  sed -i '$a GRUB_CMDLINE_LINUX="ima_hash=sha256"' /etc/default/grub
   update-grub
-
 fi
 read -p "The ima policy has been written in the file,please restart your device now[y/n] " input
 case $input in
