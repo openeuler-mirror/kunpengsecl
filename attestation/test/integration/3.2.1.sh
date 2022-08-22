c8cd34d0bd7fd672169ad81f90aaa28dff7fbbac
writuper <2018970526@qq.com>
2022-08-22 18:49:34 +0800

    update the test story 3.x

--- attestation/test/integration/3.2.1.sh
+++ attestation/test/integration/3.2.1.sh
@@ -18,9 +18,14 @@ sh clear-database.sh | tee -a ${DST}/control.txt
 popd
 ### End Preparation
 
+### define some constant
+strUUID="9b954212d796863e9f2c04372d4ab7e39fe0b62870c82a9e83c3ec326e5fb9b9"
+strDEVICE="device"
+strNAME="testDevice"
+
 ### start launching binaries for testing
 echo "start ras..." | tee -a ${DST}/control.txt
-( cd ${DST}/ras ; ./ras -T &>${DST}/ras/echo.txt ; ./ras &>>${DST}/ras/echo.txt ;)&
+( cd ${DST}/ras ; ./ras -T &>${DST}/ras/echo.txt ; ./ras -v &>>${DST}/ras/echo.txt ;)&
 echo "sleep 5s" | tee -a ${DST}/control.txt
 sleep 5
 
@@ -29,7 +34,7 @@ echo "start ${NUM} rac clients..." | tee -a ${DST}/control.txt
 (( count=0 ))
 for (( i=1; i<=${NUM}; i++ ))
 do
-    ( cd ${DST}/rac-${i} ; ${DST}/rac/raagent -t &>>${DST}/rac-${i}/echo.txt ; )&
+    ( cd ${DST}/rac-${i} ; ${DST}/rac/raagent -t -v &>>${DST}/rac-${i}/echo.txt ; )&
     (( count++ ))
     if (( count >= 1 ))
     then
@@ -43,42 +48,88 @@ echo "start to perform test ..." | tee -a ${DST}/control.txt
 echo "wait for 5s" | tee -a ${DST}/control.txt
 sleep 5
 
-# register device
-AUTHTOKEN=$(grep "Bearer " ${DST}/ras/echo.txt)
-curl -X POST -H "Authorization: $AUTHTOKEN" -H "Content-Type: application/json" http://localhost:40002/device/1234567 --data '{"id":1234567,"registered":true,"serverid":1}'
+# get cid
+echo "get client id" | tee -a ${DST}/control.txt
+cid=$(awk '{ if ($1 == "clientid:") { print $2 } }' ${DST}/rac-1/config.yaml)
+echo ${cid} | tee -a ${DST}/control.txt
 
-# get the device information
-GETDEVICE1=$(curl http://localhost:40002/device/1234567)
-echo ${GETDEVICE1} | tee -a ${DST}/control.txt
+# get the device information for the first time
+RESPONSE1=$(curl -k -H "Content-Type: application/json" https://localhost:40003/${cid}/basevalues)
+deviceINFO1=$(echo ${RESPONSE1} | jq -r '.' | grep ${strUUID})
+if [ -z "$deviceINFO1" ]
+then
+    echo "first query..." | tee -a ${DST}/control.txt
+    echo "the device named ${strNAME} does not exist." | tee -a ${DST}/control.txt
+    echo "take the next step..." | tee -a ${DST}/control.txt
+else
+    echo "the device named ${strNAME} does exist." | tee -a ${DST}/control.txt
+    pkill -u ${USER} ras
+    pkill -u ${USER} raagent
+    echo "test DONE!!!" | tee -a ${DST}/control.txt
+    exit 1
+fi
+
+echo "wait for 5s" | tee -a ${DST}/control.txt
+sleep 5
+
+# add device basevalue
+AUTHTOKEN=$(grep "Bearer " ${DST}/ras/echo.txt)
+echo "start adding new device basevalue which id is ${strUUID}..." | tee -a ${DST}/control.txt
+curl -X POST -k -H "Authorization: $AUTHTOKEN" -H "Content-Type: application/json" https://localhost:40003/${cid}/newbasevalue --data "{\"uuid\":\"${strUUID}\", \"basetype\":\"${strDEVICE}\", \"name\":\"${strNAME}\", \"enabled\":true}"
+echo "wait for 5s" | tee -a ${DST}/control.txt
+sleep 5
+
+
+# get the device information for the second time
+RESPONSE2=$(curl -k -H "Content-Type: application/json" https://localhost:40003/${cid}/basevalues)
+deviceINFO2=$(echo ${RESPONSE2} | jq -r '.' | grep ${strNAME})
+if [ -n "$deviceINFO2" ]
+then
+    echo "second query..." | tee -a ${DST}/control.txt
+    echo "the device named ${strNAME} does exist." | tee -a ${DST}/control.txt
+    echo "take the next step..." | tee -a ${DST}/control.txt
+else
+    echo "the device named ${strNAME} does not exist." | tee -a ${DST}/control.txt
+    pkill -u ${USER} ras
+    pkill -u ${USER} raagent
+    echo "test DONE!!!" | tee -a ${DST}/control.txt
+    exit 1
+fi
+
+echo "wait for 5s" | tee -a ${DST}/control.txt
+sleep 5
+
+# get bid
+echo "get basevalue id" | tee -a ${DST}/control.txt
+bid=$(echo ${RESPONSE2} | jq -r '.' | grep -B 3 ${strUUID} | awk '/"ID"/ {gsub(",","",$2);print $2}')
+echo ${bid} | tee -a ${DST}/control.txt
 
 # delete the device
-curl -X PUT -H "Authorization: $AUTHTOKEN" -H "Content-Type: application/json" http://localhost:40002/device/1234567 --data 'false'
+echo "start deleting device basevalue which id is ${strUUID}..." | tee -a ${DST}/control.txt
+curl -X DELETE -k -H "Authorization: $AUTHTOKEN" -H "Content-Type: application/json" https://localhost:40003/${cid}/basevalues/${bid}
+echo "wait for 5s" | tee -a ${DST}/control.txt
+sleep 5
 
-# get the device information
-GETDEVICE2=$(curl http://localhost:40002/device/1234567)
-echo ${GETDEVICE2} | tee -a ${DST}/control.txt
+# get the device information for the third time
+RESPONSE3=$(curl -k -H "Content-Type: application/json" https://localhost:40003/${cid}/basevalues)
+deviceINFO3=$(echo ${RESPONSE3} | jq -r '.' | grep ${strUUID})
+if [ -z "$deviceINFO3" ]
+then
+    echo "third query..." | tee -a ${DST}/control.txt
+    echo "the device named ${strNAME} does not exist." | tee -a ${DST}/control.txt
+    echo "take the next step..." | tee -a ${DST}/control.txt
+else
+    echo "the device named ${strNAME} does exist." | tee -a ${DST}/control.txt
+    pkill -u ${USER} ras
+    pkill -u ${USER} raagent
+    echo "test DONE!!!" | tee -a ${DST}/control.txt
+    exit 1
+fi
 
 ### stop testing
 echo "kill all test processes..." | tee -a ${DST}/control.txt
 pkill -u ${USER} ras
 pkill -u ${USER} raagent
 
-echo "test DONE!!!" | tee -a ${DST}/control.txt
-
-### analyse the testing data
-CLIENTID1=$(echo ${GETDEVICE1} | jq -r '.' | awk '/ID/ {gsub(",","",$2);print $2}')
-STATUS1=$(echo ${GETDEVICE1} | jq -r '.' | awk '/Deleted/ {gsub(",","",$2);gsub("\"","",$2);print $2}')
-CLIENTID2=$(echo ${GGETDEVICE2} | jq -r '.' | awk '/ID/ {gsub(",","",$2);print $2}')
-STATUS2=$(echo ${GETDEVICE2} | jq -r '.' | awk '/Deleted/ {gsub(",","",$2);gsub("\"","",$2);print $2}')
-
-### generate the test report
-echo "First time: ID:${CLIENTID1}, Status:${STATUS1}" | tee -a ${DST}/control.txt
-echo "Second time: ID:${CLIENTID2}, Status:${STATUS2}" | tee -a ${DST}/control.txt
-if [[ "${STATUS1}" == "false"  && "${STATUS2}" == "true" ]]
-then
-    echo "test succeeded!" | tee -a ${DST}/control.txt
-    exit 0
-else
-    echo "test failed!" | tee -a ${DST}/control.txt
-    exit 1
-fi
+echo "test SUCCEEDED!!!" | tee -a ${DST}/control.txt
+exit 0
--- attestation/test/integration/3.2.2.sh
+++ attestation/test/integration/3.2.2.sh
@@ -18,9 +18,16 @@ sh clear-database.sh | tee -a ${DST}/control.txt
 popd
 ### End Preparation
 
+### define some constant
+strUUID="9b954212d796863e9f2c04372d4ab7e39fe0b62870c82a9e83c3ec326e5fb9b9"
+strDEVICE="device"
+strNAME="testDevice"
+strPREIMA="db4049d7fe6443ceeedd2d2eda1f35c41d7b100a"
+strNEWIMA="0000000000000000000000000000000000000000"
+
 ### start launching binaries for testing
 echo "start ras..." | tee -a ${DST}/control.txt
-( cd ${DST}/ras ; ./ras -T &>${DST}/ras/echo.txt ; ./ras &>>${DST}/ras/echo.txt ;)&
+( cd ${DST}/ras ; ./ras -T &>${DST}/ras/echo.txt ; ./ras -v &>>${DST}/ras/echo.txt ;)&
 echo "sleep 5s" | tee -a ${DST}/control.txt
 sleep 5
 
@@ -29,7 +36,7 @@ echo "start ${NUM} rac clients..." | tee -a ${DST}/control.txt
 (( count=0 ))
 for (( i=1; i<=${NUM}; i++ ))
 do
-    ( cd ${DST}/rac-${i} ; ${DST}/rac/raagent -t &>>${DST}/rac-${i}/echo.txt ; )&
+    ( cd ${DST}/rac-${i} ; ${DST}/rac/raagent -t -v &>>${DST}/rac-${i}/echo.txt ; )&
     (( count++ ))
     if (( count >= 1 ))
     then
@@ -43,43 +50,77 @@ echo "start to perform test ..." | tee -a ${DST}/control.txt
 echo "wait for 5s" | tee -a ${DST}/control.txt
 sleep 5
 
-# register device
+# get cid
+echo "get client id" | tee -a ${DST}/control.txt
+cid=$(awk '{ if ($1 == "clientid:") { print $2 } }' ${DST}/rac-1/config.yaml)
+echo ${cid} | tee -a ${DST}/control.txt
+
+# add device basevalue
 AUTHTOKEN=$(grep "Bearer " ${DST}/ras/echo.txt)
-curl -X POST -H "Authorization: $AUTHTOKEN" -H "Content-Type: application/json" http://localhost:40002/device/1234567 --data '{"id":1234567,"registered":true,"serverid":1}'
+echo "start adding new device basevalue which uuid is ${strUUID}..." | tee -a ${DST}/control.txt
+curl -X POST -k -H "Authorization: $AUTHTOKEN" -H "Content-Type: application/json" https://localhost:40003/${cid}/newbasevalue --data "{\"uuid\":\"${strUUID}\", \"basetype\":\"${strDEVICE}\", \"name\":\"${strNAME}\", \"enabled\":true, \"ima\":\"${strPREIMA}\"}"
+echo "wait for 5s" | tee -a ${DST}/control.txt
+sleep 5
+
+# get bid
+RESPONSE=$(curl -k -H "Content-Type: application/json" https://localhost:40003/${cid}/basevalues)
+echo "get basevalue id" | tee -a ${DST}/control.txt
+bid=$(echo ${RESPONSE} | jq -r '.' | grep -B 3 ${strUUID} | awk '/"ID"/ {gsub(",","",$2);print $2}')
+echo ${bid} | tee -a ${DST}/control.txt
+
+# get the device basevalue for the first time
+BASEDEVICE1=$(curl -k -H "Content-Type: application/json" https://localhost:40003/${cid}/basevalues/${bid})
+ENABLED1=$(echo ${BASEDEVICE1} | jq -r '.' | awk '/"Enabled"/ {gsub(",","",$2);print $2}')
+DEVICEIMA1=$(echo ${BASEDEVICE1} | jq -r '.' | awk '/"Ima"/ {gsub(",","",$2);print $2}')
+if [[ $ENABLED1 == true ]]
+then
+    echo "set device named ${strNAME} basevalue succeeded!" | tee -a ${DST}/control.txt
+    echo "get the device ima value is ${DEVICEIMA1}" | tee -a ${DST}/control.txt
+    echo "take the next step..." | tee -a ${DST}/control.txt
+else
+    echo "set device named ${strNAME} basevalue failed!" | tee -a ${DST}/control.txt
+    pkill -u ${USER} ras
+    pkill -u ${USER} raagent
+    echo "test DONE!!!" | tee -a ${DST}/control.txt
+    exit 1
+fi
 
 # post basevalue
-echo "post basevalue ing..." | tee -a ${DST}/control.txt
-curl -X PUT -H "Authorization: $AUTHTOKEN" -H "Content-Type: application/json" http://localhost:40002/device/basevalue/1234567 --data '{"measurements":[{"name":"/usr/lib64/NetworkManager/1.18.8-1.el7/libnm-device-plugin-wifi.so","type":"ima","value":"db4049d7fe6443ceeedd2d2eda1f35c41d7b100a"}]}'
-# get the device basevalue
-BASEDEVICE1=$(curl http://localhost:40002/device/basevalue/1234567)
-echo ${BASEDEVICE1} | tee -a ${DST}/control.txt
-# modify basevalue
-echo "modify basevalue ing..." | tee -a ${DST}/control.txt
-curl -X PUT -H "Authorization: $AUTHTOKEN" -H "Content-Type: application/json" http://localhost:40002/device/basevalue/1234567 --data '{"measurements":[{"name":"/usr/lib64/NetworkManager/1.18.8-1.el7/libnm-device-plugin-wifi.so","type":"ima","value":"0000000000000000000000000000000000000000"}]}'
-# get the device basevalue
-BASEDEVICE2=$(curl http://localhost:40002/device/basevalue/1234567)
-echo ${BASEDEVICE2} | tee -a ${DST}/control.txt
+echo "start posting device basevalue..." | tee -a ${DST}/control.txt
+curl -X POST -k -H "Authorization: $AUTHTOKEN" -H "Content-type: application/json" https://localhost:40003/${cid}/basevalues/${bid} --data '{"enabled":false}'
+curl -X POST -k -H "Authorization: $AUTHTOKEN" -H "Content-Type: application/json" https://localhost:40003/${cid}/newbasevalue --data "{\"uuid\":\"${strUUID}\", \"basetype\":\"${strDEVICE}\", \"name\":\"${strNAME}\", \"enabled\":true, \"ima\":\"${strNEWIMA}\"}"
+echo "wait for 5s" | tee -a ${DST}/control.txt
+sleep 5
+
+# get bid
+RESPONSE2=$(curl -k -H "Content-Type: application/json" https://localhost:40003/${cid}/basevalues)
+echo "get basevalue id" | tee -a ${DST}/control.txt
+bid2=$(echo ${RESPONSE2} | jq -r '.' | grep -B 3 ${strUUID} | awk '/"ID"/ {gsub(",","",$2);print $2}' | sed -n '2p')
+echo ${bid2} | tee -a ${DST}/control.txt
+
+# get the device basevalue for the second time
+BASEDEVICE2=$(curl -k -H "Content-Type: application/json" https://localhost:40003/${cid}/basevalues/${bid})
+ENABLED2=$(echo ${BASEDEVICE2} | jq -r '.' | awk '/"Enabled"/ {gsub(",","",$2);print $2}')
+BASEDEVICE3=$(curl -k -H "Content-Type: application/json" https://localhost:40003/${cid}/basevalues/${bid2})
+ENABLED3=$(echo ${BASEDEVICE3} | jq -r '.' | awk '/"Enabled"/ {gsub(",","",$2);print $2}')
+DEVICEIMA2=$(echo ${BASEDEVICE3} | jq -r '.' | awk '/"Ima"/ {gsub(",","",$2);print $2}')
+if [[ $ENABLED2 == false && $ENABLED3 == true ]]
+then
+    echo "modify device named ${strNAME} basevalue succeeded!" | tee -a ${DST}/control.txt
+    echo "get the device ima value is ${DEVICEIMA2}" | tee -a ${DST}/control.txt
+    echo "take the next step..." | tee -a ${DST}/control.txt
+else
+    echo "modify device named ${strNAME} basevalue failed!" | tee -a ${DST}/control.txt
+    pkill -u ${USER} ras
+    pkill -u ${USER} raagent
+    echo "test DONE!!!" | tee -a ${DST}/control.txt
+    exit 1
+fi
 
 ### stop testing
 echo "kill all test processes..." | tee -a ${DST}/control.txt
 pkill -u ${USER} ras
 pkill -u ${USER} raagent
 
-echo "test DONE!!!" | tee -a ${DST}/control.txt
-
-### analyse the testing data
-
-STATUS1=$(echo ${BASEDEVICE1} | grep "db4049d7fe6443ceeedd2d2eda1f35c41d7b100a")
-STATUS2=$(echo ${BASEDEVICE2} | grep "0000000000000000000000000000000000000000")
-
-### generate the test report
-echo "First time: basevalue:${STATUS1}" | tee -a ${DST}/control.txt
-echo "Second time: basevalue:${STATUS2}" | tee -a ${DST}/control.txt
-if [[ "${STATUS1}" && "${STATUS2}" ]]
-then
-    echo "test succeeded!" | tee -a ${DST}/control.txt
-    exit 0
-else
-    echo "test failed!" | tee -a ${DST}/control.txt
-    exit 1
-fi
+echo "test SUCCEEDED!!!" | tee -a ${DST}/control.txt
+exit 0
