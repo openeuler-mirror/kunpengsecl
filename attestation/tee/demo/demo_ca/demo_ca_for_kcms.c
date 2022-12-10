ac25355470709b38663c29ed6f7f36e648196040
leezhenxiang <1172294056@qq.com>
2022-12-10 21:20:46 +0800

    add ca_demo and ta_demo, fix kcml

---
+++ attestation/tee/demo/demo_ca/demo_ca_for_kcms.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include "tee_client_api.h"
+
+#define OPERATION_START_FLAG 1
+#define MAX_STR_LEN 64
+#define SYMBOL_PARAM 3
+#define VALUE_INIT 0x8fffffff
+
+static const TEEC_UUID Uuid = {
+    0xbbb2d138, 0xee21, 0x43af, { 0x87, 0x96, 0x40, 0xc2, 0x0d, 0x7b, 0x45, 0xfa }
+};
+
+enum {
+    CMD_DATA_ENCRIPT = 0x01, //a scene which needs ta to encrypt some data
+    CMD_TA_CALLBACK  = 0x02, //a scene which ta needs to be call back
+    CMD_TA_EXIT      = 0x03, //a scene which ta exits and needs to clear its info in kta
+};
+
+int main(){
+    TEEC_Context context = {0};
+    TEEC_Session session = {0};
+    TEEC_Operation operation = {0};
+    uint32_t origin = 0;
+    TEEC_Result result;
+    int32_t i = 0;
+
+    result = TEEC_InitializeContext(NULL, &context);
+    if (result != TEEC_SUCCESS) {
+        printf("initial context failed");
+        goto end1;
+    }
+
+    context.ta_path = "/root/data/bbb2d138-ee21-43af-8796-40c20d7b45fa.sec";
+    operation.started = OPERATION_START_FLAG;
+    operation.paramTypes = TEEC_PARAM_TYPES(
+        TEEC_NONE,
+        TEEC_NONE,
+        TEEC_NONE,
+        TEEC_NONE);
+
+    result = TEEC_OpenSession(&context, &session, &Uuid, TEEC_LOGIN_IDENTIFY, NULL, &operation, &origin);
+    if (result != TEEC_SUCCESS) {
+        printf("open session failed");
+        goto end2;
+    }
+    //Demonstrate twice, first for key generation, second for key search
+    for(; i < 2; i++) {
+        operation.started = OPERATION_START_FLAG;
+        operation.paramTypes = TEEC_PARAM_TYPES(
+            TEEC_NONE,
+            TEEC_NONE,
+            TEEC_NONE,
+            TEEC_VALUE_OUTPUT //we need one parameter to identify whether needs ca to call back
+            );
+        operation.params[SYMBOL_PARAM].value.a = VALUE_INIT; //a marks whether ta needs needs to be called back, a=0 means not need
+        result = TEEC_InvokeCommand(&session, CMD_DATA_ENCRIPT, &operation, &origin);
+        if(result != TEEC_SUCCESS) {
+            printf("encrypt data process failed, codes=0x%x, origin=0x%x", result, origin);
+            goto end3;
+        }
+        if(operation.params[SYMBOL_PARAM].value.a == 0) {
+            printf("encrypt data process succeeded");
+            goto else_options;
+        } else if (operation.params[SYMBOL_PARAM].value.a != 1) {
+            printf("encrypt data process failed, parameter is wrong");
+            goto end3;
+        } else {
+            printf("ta needs to be called back, wait 2s");
+            sleep(2);
+            operation.started = OPERATION_START_FLAG;
+            operation.paramTypes = TEEC_PARAM_TYPES(
+                TEEC_NONE,
+                TEEC_NONE,
+                TEEC_NONE,
+                TEEC_NONE
+            );
+            result = TEEC_InvokeCommand(&session, CMD_TA_CALLBACK, &operation, &origin);
+            if(result != TEEC_SUCCESS) {
+                printf("ta call back failed, codes=0x%x, origin=0x%x", result, origin);
+                goto end3;
+            }
+            printf("ta call back success");
+        }
+    }
+
+//other options to be executed, here use ta exit as an example
+else_options:
+    operation.started = OPERATION_START_FLAG;
+    operation.paramTypes = TEEC_PARAM_TYPES(
+        TEEC_NONE,
+        TEEC_NONE,
+        TEEC_NONE,
+        TEEC_NONE
+        );
+    result = TEEC_InvokeCommand(&session, CMD_TA_EXIT, &operation, &origin);
+    if(result != TEEC_SUCCESS) {
+        printf("ta exit failed, codes=0x%x, origin=0x%x", result, origin);
+        goto end3;
+    }
+    printf("ta exit succeeded");
+
+end3:
+    TEEC_CloseSession(&session);
+end2:
+    TEEC_FinalizeContext(&context);
+end1:
+    return 0;
+}
---
+++ attestation/tee/demo/demo_ta/demo_ta_for_kcms.c
@@ -0,0 +1,349 @@
+#include <stdlib.h>
+#include <tee_ext_api.h>
+#include <tee_log.h>
+#include <securec.h>
+#include "tee_trusted_storage_api.h"
+#include "tee_time_api.h"
+#include "tee_core_api.h"
+#include "kcml.h"
+
+#define PARAM_COUNT   4
+#define MAX_STR_LEN 64
+#define PARAMETER_FRIST 0
+#define PARAMETER_SECOND 1
+#define PARAMETER_THIRD 2
+#define PARAMETER_FOURTH 3
+
+enum {
+    CMD_DATA_ENCRIPT = 0x01,
+    CMD_TA_CALLBACK  = 0x02,
+    CMD_TA_EXIT      = 0x03,
+};
+
+static const TEE_UUID localUuid = {
+    0xbbb2d138, 0xee21, 0x43af, { 0x87, 0x96, 0x40, 0xc2, 0x0d, 0x7b, 0x45, 0xfa }
+};
+static const TEE_UUID ktaUuid = {
+    0x435dcafa, 0x0029, 0x4d53, { 0x97, 0xe8, 0xa7, 0xa1, 0x3a, 0x80, 0xc8, 0x2e }
+};
+uint32_t new_key_flag = 1;
+
+uint8_t *account = NULL;
+uint8_t *password = NULL;
+TEE_UUID masterkey = {0};
+
+typedef struct _StatusFlag {
+    TEE_UUID *keyid;
+    //A symbol variable which indicates which operetion to be execute after ca call back.
+    //0 means no need to reply
+    //1 means get key generation reply
+    //2 means get key search reply
+    uint32_t symbol;
+} StatusFlag;
+
+StatusFlag flag = {NULL, 0};
+void *keyid_storage_path = "sec_storage_data/takeyid.txt";
+
+void hex2char(uint32_t hex, int8_t *hexchar, int32_t i) {
+    for(i--; i >= 0; i--, hex >>= 4) {
+        if ((hex & 0xf) <= 9)
+            *(hexchar + i) = (hex & 0xf) + '0';
+        else
+            *(hexchar + i) = (hex & 0xf) + 'A' - 0x0a;
+    }
+}
+
+void uuid2char(TEE_UUID uuid, int8_t charuuid[37]) {
+    int32_t i = 0;
+
+    hex2char(uuid.timeLow, charuuid, 8);
+    hex2char(uuid.timeMid, charuuid + 9, 4);
+    hex2char(uuid.timeHiAndVersion, charuuid + 14, 4);
+    for(i = 0; i < 2; i++){
+        hex2char(uuid.clockSeqAndNode[i], charuuid + 19 + i * 2, 2);
+    }
+    for(i = 0; i < 6; i++){
+        hex2char(uuid.clockSeqAndNode[i+2], charuuid + 24 + i * 2, 2);
+    }
+}
+
+void char2uuid(TEE_UUID *uuid, int8_t charuuid[37]) {
+    int32_t i = 0;
+    int8_t *stop;
+    int8_t buffer[3];
+    uuid->timeLow = strtoul(charuuid, &stop, 16);
+    uuid->timeMid = strtoul(stop + 1, &stop, 16);
+    uuid->timeHiAndVersion = strtoul(stop + 1, &stop, 16);
+    for(i = 0; i < 2; i++) {
+        uuid->clockSeqAndNode[i] = strtoul(charuuid + 19 + i * 2, &stop, 16) >> (8 - i * 8);
+    }
+    for(i = 0; i < 6; i++) {
+        buffer[0] = *(charuuid + 24 + i * 2);
+        buffer[1] = *(charuuid + 25 + i * 2);
+        uuid->clockSeqAndNode[i + 2] = strtoul(buffer, &stop, 16);
+    }
+}
+
+//The encryption operation or other operations needs key
+TEE_Result encrypt(uint8_t *keyvalue) {
+    uint8_t *data = "demo data";
+    (void*)keyvalue;
+}
+
+//
+TEE_Result encrypt_data_pre(uint32_t param_type, TEE_Param params[PARAM_COUNT]) {
+    TEE_Result ret;
+    uint8_t *keyvalue = NULL;
+    //A flag indicating whether a new key is required
+    TEE_UUID keyid = {0};
+    TEE_ObjectHandle keyid_data = NULL;
+    uint32_t len = 36, count = 0;
+    int8_t *keyidchar = NULL;
+
+    if (!check_param_type(param_type,
+        TEE_PARAM_TYPE_NONE,
+        TEE_PARAM_TYPE_NONE,
+        TEE_PARAM_TYPE_NONE,
+        TEE_PARAM_TYPE_VALUE_OUTPUT)) {
+        tloge("Bad expected parameter types, 0x%x.\n", param_type);
+        return TEE_ERROR_BAD_PARAMETERS;
+    }
+    if (params[PARAMETER_FOURTH].value.a != 0x8fffffff) {
+        tloge("Bad expected parameter value");
+        return TEE_ERROR_BAD_PARAMETERS;
+    }
+
+    tlogd("prepare to encrypt data");
+    uint32_t twice_search_flag = 0;
+
+    if(new_key_flag == 1) {
+        ret = generate_key(&localUuid, account, password, &masterkey);
+        if(ret != TEE_SUCCESS) {
+            tloge("generate key command failed");
+            return ret;
+        }
+        params[PARAMETER_FOURTH].value.a = 1;
+        flag.symbol = 1;
+        new_key_flag -= 1;
+        tlogd("prepare to encrypt data success");
+        return TEE_SUCCESS;
+    }
+    if(new_key_flag == 0) {
+        ret = TEE_OpenPersistentObject(TEE_OBJECT_STORAGE_PRIVATE, keyid_storage_path, strlen(keyid_storage_path),
+                TEE_DATA_FLAG_ACCESS_READ, &keyid_data);
+        if (ret != TEE_SUCCESS) {
+            tloge("failed to open file:ret = 0x%x\n", ret);
+            return ret;
+        }
+        keyidchar = TEE_Malloc(len + 1, 0);
+        if (keyidchar == NULL) {
+            tloge("failed to open file:ret = 0x%x\n", ret);
+            TEE_CloseObject(keyid_data);
+            return ret;
+        }
+        ret = TEE_ReadObjectData(keyid_data, keyidchar, len, &count);
+        if (ret != TEE_SUCCESS) {
+            TEE_CloseObject(keyid_data);
+            TEE_Free(keyidchar);
+        return ret;
+        }
+        TEE_CloseObject(keyid_data);
+        char2uuid(&keyid, keyidchar);
+
+        ret = search_key(&localUuid, account, password, &keyid, &masterkey, keyvalue, &twice_search_flag);
+        if(ret != TEE_SUCCESS) {
+            tloge("search command failed");
+            return ret;
+        }
+        if(twice_search_flag) {
+            params[PARAMETER_FOURTH].value.a = 1;
+            flag.keyid = &keyid;
+            flag.symbol = 2;
+            tlogd("prepare to encrypt data success");
+            return TEE_SUCCESS;
+        }
+        encrypt(keyvalue);
+        tlogd("encrypt data success");
+        return TEE_SUCCESS;
+    }
+}
+
+TEE_Result call_back(uint32_t param_type, TEE_Param params[PARAM_COUNT]) {
+    TEE_Result ret;
+    TEE_UUID keyid = {0};
+    int8_t keyidchar[37] = "------------------------------------";
+    int8_t *keyiddelete = NULL;
+    uint8_t *keyvalue = NULL;
+    TEE_ObjectHandle keyid_data = NULL;
+        uint32_t len = 36, count = 0;
+
+    if (!check_param_type(param_type,
+        TEE_PARAM_TYPE_NONE,
+        TEE_PARAM_TYPE_NONE,
+        TEE_PARAM_TYPE_NONE,
+        TEE_PARAM_TYPE_NONE)) {
+        tloge("Bad expected parameter types, 0x%x.\n", param_type);
+        return TEE_ERROR_BAD_PARAMETERS;
+    }
+
+    tlogd("start to encrypt data");
+    uint32_t twice_search_flag;
+    switch(flag.symbol) {
+    case 1:
+        ret = get_key_reply(&localUuid, account, password, &keyid, &masterkey, keyvalue);
+        if (ret != TEE_SUCCESS) {
+            tloge("get generate key reply failed");
+            return ret;
+        }
+        uuid2char(keyid, keyidchar);
+        ret = TEE_CreatePersistentObject(TEE_OBJECT_STORAGE_PRIVATE, keyid_storage_path, strlen(keyid_storage_path),
+                TEE_DATA_FLAG_ACCESS_WRITE, TEE_HANDLE_NULL, NULL, 0, &keyid_data);
+        if (ret != TEE_SUCCESS) {
+            tloge("Failed to create file: ret = 0x%x\n", ret);
+            return ret;
+        }
+
+        ret = TEE_WriteObjectData(keyid_data, keyidchar, strlen(keyidchar));
+        if (ret != TEE_SUCCESS) {
+            tloge("Failed to write file: ret = 0x%x\n", ret);
+            TEE_CloseObject(keyid_data);
+            return ret;
+        }
+
+        TEE_CloseObject(keyid_data);
+        break;
+    case 2:
+        ret = search_key(&localUuid, account, password, &keyid, &masterkey, keyvalue, &twice_search_flag);
+        if (ret != TEE_SUCCESS) {
+            tloge("get generate key reply failed");
+            return ret;
+        }
+        if(twice_search_flag) {
+            tloge("not find designated key");
+            return TEE_ERROR_TIMEOUT;
+        }
+        break;
+    default:
+        tloge("parameter symbol is wrong");
+        return TEE_ERROR_BAD_PARAMETERS;
+    }
+    encrypt(keyvalue);
+    tlogd("encrypt data success");
+
+    //a delete key attribute example
+    if(new_key_flag == 0) {
+        ret = TEE_OpenPersistentObject(TEE_OBJECT_STORAGE_PRIVATE, keyid_storage_path, strlen(keyid_storage_path),
+                TEE_DATA_FLAG_ACCESS_READ, &keyid_data);
+        if (ret != TEE_SUCCESS) {
+            tloge("failed to open file:ret = 0x%x\n", ret);
+            return ret;
+        }
+        keyiddelete = TEE_Malloc(len + 1, 0);
+        if (keyiddelete == NULL) {
+            tloge("failed to open file:ret = 0x%x\n", ret);
+            TEE_CloseObject(keyid_data);
+            return ret;
+        }
+        ret = TEE_ReadObjectData(keyid_data, keyiddelete, len, &count);
+        if (ret != TEE_SUCCESS) {
+            TEE_CloseObject(keyid_data);
+            TEE_Free(keyiddelete);
+        return ret;
+        }
+        ret = delete_key(&localUuid, account, password, keyiddelete);
+        if (ret != TEE_SUCCESS) {
+            tloge("delete key failed");
+            return ret;
+        }
+    }
+    tlogd("execute call back success");
+    return TEE_SUCCESS;
+}
+
+TEE_Result ta_exit(uint32_t param_type, TEE_Param params[PARAM_COUNT]) {
+    TEE_Result ret;
+    TEE_ObjectHandle keyid_data = NULL;
+
+    ret = clear_cache(&localUuid, account, password);
+    if(ret != TEE_SUCCESS) {
+        tloge("clear cache failed");
+        return ret;
+    }
+    ret = TEE_OpenPersistentObject(TEE_OBJECT_STORAGE_PRIVATE, keyid_storage_path, strlen(keyid_storage_path),
+            TEE_DATA_FLAG_ACCESS_WRITE_META, &keyid_data);
+    if (ret != TEE_SUCCESS) {
+        tloge("failed to open file:ret = 0x%x\n", ret);
+        return ret;
+    }
+    TEE_CloseAndDeletePersistentObject(keyid_data);
+    tlogd("ta exit preparation success");
+    return TEE_SUCCESS;
+}
+
+TEE_Result TA_CreateEntryPoint(void)
+{
+    TEE_Result ret;
+    ret = addcaller_ca_exec("/vendor/bin/demo_hello", "root");
+    if (ret != TEE_SUCCESS) {
+        tloge("add caller ca failed");
+        return TEE_ERROR_GENERIC;
+    }
+    tlogd("add caller ca success");
+    return TEE_SUCCESS;
+}
+
+TEE_Result TA_OpenSessionEntryPoint(uint32_t parm_type,
+    TEE_Param params[PARAM_COUNT], void** session_context)
+{
+    (void)parm_type;
+    (void)params;
+    (void)session_context;
+    tlogd("open session success");
+
+    return TEE_SUCCESS;
+}
+
+TEE_Result TA_InvokeCommandEntryPoint(void* session_context, uint32_t cmd,
+    uint32_t param_type, TEE_Param params[PARAM_COUNT])
+{
+    TEE_Result ret;
+    (void)session_context;
+
+    tlogd("---- TA invoke command ----------- ");
+    switch (cmd) {
+    case CMD_DATA_ENCRIPT:
+        ret = encrypt_data_pre(param_type, params);
+        if(ret != TEE_SUCCESS) {
+            tloge("encrypt data process failed0x%x", ret);
+            return ret;
+        }
+        break;
+    case CMD_TA_CALLBACK:
+        ret = call_back(param_type, params);
+        if(ret != TEE_SUCCESS) {
+            tloge("execute call back operation failed0x%x", ret);
+            return ret;
+        }
+        break;
+    case CMD_TA_EXIT:
+        ret = ta_exit(param_type, params);
+        if(ret != TEE_SUCCESS) {
+            tloge("execute call back operation failed0x%x", ret);
+            return ret;
+        }
+        break;
+    default:
+        tloge("Unknown cmd is %u", cmd);
+        ret = TEE_ERROR_BAD_PARAMETERS;
+    }
+    return  ret;
+}
+
+void TA_CloseSessionEntryPoint(void* session_context){
+    (void)session_context;
+    tlogd("---- close session ----- ");
+}
+
+void TA_DestroyEntryPoint(void){
+    tlogd("---- destory TA ---- ");
+}
